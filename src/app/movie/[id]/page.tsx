4a5df8407f21cef1eaee14ee181f3cd8ac2dbec0
Yongyeon Kim <seyeon.dev@gmail.com>
2025-01-15 16:16:59 +0900

    feat: add dynamic metadata

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -1,8 +1,24 @@
+import type { Metadata } from "next";
 import { Suspense } from "react";
 
+import { getMovieDetail } from "@/api/movie";
 import MovieInfo from "@/components/MovieInfo";
 import MovieVideos from "@/components/MovieVideos";
 import styles from "@/styles/movie-detail.module.css";
+
+type Props = {
+  params: Promise<{ id: string }>;
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  // read route params
+  const id = (await params).id;
+  const movie = await getMovieDetail(id);
+
+  return {
+    title: movie.title,
+  };
+}
 
 export default async function MovieDetailPage({ params }: { params: Promise<{ id: string }> }) {
   const id = (await params).id;
